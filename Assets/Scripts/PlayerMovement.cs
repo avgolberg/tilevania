d2c48033957ad753dfc7caaa523a9a56badf8d91
Alyona Berezorutska <av.golberg@gmail.com>
2026-01-08 17:29:13 +0200

    Add ladder climbing

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,24 +6,28 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 8f;
+    [SerializeField] float climbSpeed = 5f;
     [SerializeField] float jumpSpeed = 25f;
-    
+
     Vector2 moveInput;
     Rigidbody2D rb;
     Animator animator;
     CapsuleCollider2D capsuleCollider;
+    float gravityScaleAtStart;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         capsuleCollider = GetComponent<CapsuleCollider2D>();
+        gravityScaleAtStart = rb.gravityScale;
     }
 
     void Update()
     {
         Run();
         FlipSprite();
+        ClimbLadder();
     }
 
     void OnMove(InputValue value)
@@ -35,7 +39,7 @@ public class PlayerMovement : MonoBehaviour
     {
         if (!capsuleCollider.IsTouchingLayers(LayerMask.GetMask("Ground"))) return;
         if (value.isPressed)
-            rb.linearVelocity += new Vector2(0f, jumpSpeed);
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y + jumpSpeed);
     }
 
     void Run()
@@ -51,5 +55,22 @@ public class PlayerMovement : MonoBehaviour
         bool isMoving = Mathf.Abs(rb.linearVelocity.x) > Mathf.Epsilon;
         if (isMoving)
             transform.localScale = new Vector2(Mathf.Sign(rb.linearVelocity.x), 1f);
+    }
+
+    void ClimbLadder()
+    {
+        if (!capsuleCollider.IsTouchingLayers(LayerMask.GetMask("Ladder")))
+        {
+            rb.gravityScale = gravityScaleAtStart;
+            animator.SetBool("isClimbing", false);
+            return;
+        }
+        
+        rb.gravityScale = 0f;
+        Vector2 playerVelocity = new Vector2(rb.linearVelocity.x, moveInput.y * climbSpeed);
+        rb.linearVelocity = playerVelocity;
+
+        bool isClimbing = Mathf.Abs(rb.linearVelocity.y) > Mathf.Epsilon;
+        animator.SetBool("isClimbing", isClimbing);
     }
 }
