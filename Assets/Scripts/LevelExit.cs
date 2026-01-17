b0a1cd76174fb187ea0720ba512eb4e394fd7709
Alyona Berezorutska <av.golberg@gmail.com>
2026-01-17 18:33:35 +0200

    Add initial levels and level loading

---
+++ Assets/Scripts/LevelExit.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        int index = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(index + 1);
+    }
+}
